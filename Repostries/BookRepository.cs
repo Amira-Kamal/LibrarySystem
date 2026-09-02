429050608ff3840df41a8ed1edae41d049f56dc5
Afnan103 <afnaneldeeb1@gmail.com>
2026-09-03 00:42:31 +0300

    add BookRepository

---
+++ Repostries/BookRepository.cs
@@ -0,0 +1,76 @@
+﻿using LibrarySystem.Data;
+using LibrarySystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrarySystem.Repostries
+{
+    public class BookRepository
+    {
+        private readonly LibraryDbContext _context;
+
+        public BookRepository(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Book>> GetAllAsync()
+        {
+            return await _context.Books
+                .Include(b => b.Category)
+                .Include(b => b.Author)
+                .ToListAsync();
+        }
+
+
+        public async Task<List<Book>> GetAvailableAsync()
+        {
+            return await _context.Books
+                .Include(b => b.Category)
+                .Include(b => b.Author)
+                .Where(b => b.AvailableCopies > 0)
+                .ToListAsync();
+        }
+
+        public async Task<Book?> GetByIdAsync(int id)
+        {
+            return await _context.Books
+                .Include(b => b.Category)
+                .Include(b => b.Author)
+                .FirstOrDefaultAsync(b => b.BookId == id);
+        }
+
+        public async Task<Book?> FindAsync(int id)
+        {
+            return await _context.Books
+                .FindAsync(id);
+        }
+
+        public async Task AddAsync(Book book)
+        {
+            await _context.Books.AddAsync(book);
+
+            await _context.SaveChangesAsync();
+        }
+
+
+
+        public async Task UpdateAsync(Book book)
+        {
+            _context.Books.Update(book);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Repostries/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using LibrarySystem.Models;
+
+namespace LibrarySystem.Repostries
+{
+    public interface IBookRepository
+    {
+        Task<List<Book>> GetAllAsync();
+        Task<List<Book>> GetAvailableAsync();
+        Task<Book?> GetByIdAsync(int id);
+        Task<Book?> FindAsync(int id);
+        Task AddAsync(Book book);
+        Task UpdateAsync(Book book);
+        Task DeleteAsync(int id);
+    }
+}
